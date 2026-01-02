d15ad942645a2f18da653ae00815a4e00c584d24
Logan McLintock <loganpkg@gmail.com>
2026-01-03 03:03:06 +1030

    switch to the CDDL-1.1

--- swap_license.sh
+++ swap_license.sh
@@ -1,28 +1,23 @@
 #! /bin/sh
 
 #
-# Copyright (c) 2025 Logan Ryan McLintock. All rights reserved.
+# Copyright (c) 2025, 2026 Logan Ryan McLintock. All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
+# The contents of this file are subject to the
+# Common Development and Distribution License (CDDL) version 1.1
+# (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License in
+# the LICENSE file included with this software or at
+# https://opensource.org/license/cddl-1-1
 #
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
+# NOTICE PURSUANT TO SECTION 4.2 OF THE LICENSE:
+# This software is prohibited from being distributed or otherwise made
+# available under any subsequent version of the License.
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
 #
 
 # A simple script to assist in changing the license of a software project.
